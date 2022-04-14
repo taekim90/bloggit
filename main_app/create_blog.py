703bf28e29257a5d420937beab75a8a775b21b24
taekim90 <taehk15@gmail.com>
2022-04-13 21:50:50 -0700

    now able to edit and delete post when in a specific blog

--- main_app/create_blog.py
+++ main_app/create_blog.py
@@ -1,15 +1,12 @@
 from django import forms
 from .models import Blog
 
-# from django.contrib.auth.forms import UserCreationForm
-# from django.contrib.auth.models import User
-
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ['user', 'title', 'content']
-
-# class SignUpForm(UserCreationForm):
-#     class Meta:
-#         model = User
-#         fields = ('username', 'password1', 'password2', )
+        widgets = {
+            'user': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': "user", 'type': 'hidden'}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+        }
