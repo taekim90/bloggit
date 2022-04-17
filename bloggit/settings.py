af7ad5ed16e7ba42f5abed2a166e62f259b3b321
taekim90 <taehk15@gmail.com>
2022-04-17 16:38:07 -0700

    heroku

--- bloggit/settings.py
+++ bloggit/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (os.path.join( os.path.dirname( __file__ ), 'static' ),)
-# STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'bloggit/static')]
 
 STATICFILES_FINDERS = (
 'django.contrib.staticfiles.finders.FileSystemFinder',
