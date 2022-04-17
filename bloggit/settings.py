e6a8f17521ed4a36a62774c48288dd672f30d025
taekim90 <taehk15@gmail.com>
2022-04-17 16:39:35 -0700

    changes to settings

--- bloggit/settings.py
+++ bloggit/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (os.path.join( os.path.dirname( __file__ ), 'static' ),)
+# STATICFILES_DIRS = (os.path.join( os.path.dirname( __file__ ), 'static' ),)
 # STATICFILES_DIRS = [os.path.join(BASE_DIR,'bloggit/static')]
 
 STATICFILES_FINDERS = (
